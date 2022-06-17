2ff960390466700ddea48622a646ebc2d66b3a09
Nelson Taban <nelson.lobojo@strathmore.edu>
2022-06-17 19:42:55 +0300

    decrypting

--- server/server.js
+++ server/server.js
@@ -7,8 +7,14 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-// environment decoding using dot env
+// environment variables decoding using dot env
 dotenv.config();
+/*
+//imports for sending message via Twilio
+const accountSid = process.env.REACT_APP_TWILIO_ACCOUNT_SID;
+const authToken = process.env.REACT_APP_TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+*/
 
 // building the path
 const buildPath = path.join(__dirname, '..', 'build');
@@ -25,17 +31,29 @@ function encrypt3DES(data, key) {
   encrypted += cipher.final('base64');
   return encrypted;
 }
+// function for decrypting uisng DES algorithm using crypto
+function decrypt3DES(data, key) {
+  const md5Key = crypto.createHash('md5').update(key).digest("hex").substr(0, 24);
+  const decipher = crypto.createDecipheriv('des-ede3', md5Key, '');
+
+  let encrypted = decipher.update(data, 'base64', 'utf8');
+  encrypted += decipher.final('utf8');
+  return encrypted;
+}
 
 // security key that will be shared with the client via otp and encryption
 
 const securityKey = crypto.randomBytes(32);
+console.log(securityKey);
 
+/*
 // email sending function
 
 app.post('/send', (req, res) => {
 
   // encrypt the file
   const message = req.body.message;
+  const phoneNumber = req.body.phoneNumber;
   const encryptedMessage = encrypt3DES(message, securityKey);
 
   // prepare the mail options to be sent via nodemailer
@@ -47,6 +65,13 @@ app.post('/send', (req, res) => {
       html: `<p>Your encrypted message:</p>
              <p>${encryptedMessage}</p>`,
     };
+
+    client.messages
+      .create({
+        body: "Your security key: " + securityKey,
+        from: process.env.REACT_APP_TWILIO_PHONE_NUMBER,
+        to: phoneNumber
+      })
 
     // function for sending the email
     transporter.sendMail(mailOptions, function (err, info) {
@@ -70,9 +95,42 @@ app.post('/send', (req, res) => {
   }
 });
 
+app.post('/decrypt', (req, res) => {
+
+  // encrypt the file
+  const message = req.body.encryptedMessage.toString();
+  const decryptionKey = req.body.decryptionKey;
+  const decryptedptedMessage = decrypt3DES(message, decryptionKey);
+
+  // prepare the mail options to be sent via nodemailer
+  try {
+    function sendDecryptedMessage(error, message) {
+      if(error) {
+        res.status(500).send({
+          success: false,
+          message: 'Something went wrong. Try again later'
+        });
+      } else{
+        res.send({
+          success: true,
+          message: message
+        });
+      }
+     
+    }  
+    sendDecryptedMessage(null, decryptedptedMessage);
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: 'Something went wrong. Try again later'
+    });
+  }
+});
+
 
 app.listen(3030, () => {
   console.log('server start on port 3030');
 });
+*/
 
 
--- src/EncryptionPage.js
+++ src/EncryptionPage.js
@@ -8,10 +8,11 @@ export const EncryptionPage = () => {
     name: '',
     email: '',
     message: '',
-    phonenumber: ''
+    phoneNumber: ''
   });
   // state to handle result of the email sending
   const [result, setResult] = useState(null);
+  const [decryptedMessage, setDecryptedMessage] = useState(null);
 
   // code to trigger Sending email
   const sendEmail = event => {
@@ -20,12 +21,27 @@ export const EncryptionPage = () => {
       .post('/send', { ...state })
       .then(response => {
         setResult(response.data);
-        setState({ name: '', email: '', message: '', phonenumber: '' });
+        setState({ name: '', email: '', message: '', phoneNumber: '' });
       })
       .catch(() => {
         setResult({ success: false, message: 'Something went wrong. Please try again.' });
       });
-    
+
+  };
+
+  // code to decrypt message
+  const decrypt = event => {
+    event.preventDefault();
+    axios
+      .post('/decrypt', { ...state })
+      .then(response => {
+        setDecryptedMessage(response.data);
+        setState({ encryptedMessage: '', decryptionKey: '' });
+      })
+      .catch(() => {
+        setResult({ success: false, message: 'Something went wrong. Please try again.' });
+      });
+
   };
 
   const onInputChange = event => {
@@ -44,15 +60,16 @@ export const EncryptionPage = () => {
           <img src={encrypt} alt="..." className="shadow-lg rounded-full max-w-full h-auto align-middle border-none" />
         </div>
 
-        // result to see whether email is sent or not
+
         {result && (
-          <p className={`${result.success ? 'success': 'error'}`}>
+          <p className={`${result.success ? 'success' : 'error'}`}>
             {result.message}
           </p>
         )}
 
 
         <form className="w-full max-w-lg m-auto py-10 mt-10 px-10 border" onSubmit={sendEmail}>
+          <h1>Encryption Part</h1>
           <div>
             <label className="text-gray-600 font-medium">Name:</label>
             <input className="border-solid border-gray-300 border py-2 px-4 w-full
@@ -66,10 +83,10 @@ export const EncryptionPage = () => {
               value={state.email} onChange={onInputChange} required />
           </div>
           <div>
-            <label className="text-gray-600 font-medium" htmlFor="phonenumber">Phone Number:</label>
+            <label className="text-gray-600 font-medium" htmlFor="phoneNumber">Phone Number:</label>
             <input className="border-solid border-gray-300 border py-2 px-4 w-full
-        rounded text-gray-700" type="tel" name="phonenumber" placeholder='Must start with +254'
-              value={state.phonenumber} onChange={onInputChange} required />
+        rounded text-gray-700" type="tel" name="phoneNumber" placeholder='Must start with +254'
+              value={state.phoneNumber} onChange={onInputChange} required />
           </div>
           <div>
             <label className="text-gray-600 font-medium" htmlFor="message">Message:</label>
@@ -80,8 +97,40 @@ export const EncryptionPage = () => {
           <button className="mt-4 w-full bg-green-400 hover:bg-green-600 text-green-100 border py-3 px-6 font-semibold text-md rounded"
             type="submit">Submit</button>
         </form>
+
+
+
+        <form className="w-full max-w-lg m-auto py-10 mt-10 px-10 border" onSubmit={decrypt}>
+          <h1>Decryption Page</h1>
+          <div>
+            <label className="text-gray-600 font-medium" htmlFor="message"> Encrypted Message:</label>
+            <textarea className="border-solid border-gray-300 border py-20 px-4 w-full
+        rounded text-gray-700" rows={3} cols={5} name="encryptedMessage" placeholder='Message'
+              value={state.encryptedMessage} onChange={onInputChange} required />
+          </div>
+          <div>
+            <label className="text-gray-600 font-medium" htmlFor="phoneNumber">Decryption Key:</label>
+            <input className="border-solid border-gray-300 border py-2 px-4 w-full
+        rounded text-gray-700" type="text" name="decryptionKey" placeholder='Must start with +254'
+              value={state.decryptionKey} onChange={onInputChange} required />
+          </div>
+          <button className="mt-4 w-full bg-green-400 hover:bg-green-600 text-green-100 border py-3 px-6 font-semibold text-md rounded"
+            type="submit">Submit</button>
+        </form>
+
+        <div>
+          <label className="text-gray-600 font-medium" htmlFor="message"> Decrypted Message:</label>
+          {decryptedMessage && (
+            <p className={`${decryptedMessage.success ? 'success' : 'error'}`}>
+              {decryptedMessage.message}
+            </p>
+          )}
+
+        </div>
       </div>
     </div>
   );
 
 }
+
+export default EncryptionPage;
